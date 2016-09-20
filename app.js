e4f53c3789c7a77005d3d6b46cbda2ad543fdc87
Andres Aguero <andy_andres@live.com>
2016-09-19 21:20:13 -0700

    Fullscreen Youtube showing when hit play, still needs some resolving will look into tutorial in header-video folder tomorrow

--- app.js
+++ app.js
@@ -34,7 +34,38 @@ HeaderVideo.prototype.getVideoDetails = function() {
 
 	return {
 		videoURL: mediaElement.attr('data-video-URL')
+		teaser: mediaElement.attr('data-teaser'),
 		videoHeight: mediaElement.attr('data-video-height'),
         videoWidth: mediaElement.attr('data-video-width')	
     };
 };
+
+HeaderVideo.prototype.setFluidContainer = function () {
+	var element = this.$element;
+	element.data('aspectRation', this.videoDetails.videoHeight / this.videoDetails.videoWidth);
+
+	$(window).resize(function() {
+		var windowWidth = $(window).width();
+		var windowHeight = $(window).height();
+
+		element.width(Math.ceil(windowWidth));
+		element.height(Math.ceil(windowWidth * element.data('aspectRation')));
+
+		if(windowHeight < elementHeight()) {
+			element.width(Math.ceil(windowWidth));
+			element.height(Math.ceil(windowHeight));
+		}
+	}).trigger('resize');
+
+HeaderVideo.prototype.appendIframe = function() {
+    var html = '<iframe id="header-video__video-element" src="'+this.videoDetails.videoURL+'?rel=0&hd=1&autohide=1&showinfo=0&autoplay=1&enablejsapi=1&origin=*" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+    this.$element.append(html);
+};
+
+HeaderVideo.prototype.appendTeaserVideo = function() {
+    var source = this.videoDetails.teaser;
+    var html = '<video autoplay="true" loop="true" muted id="header-video__teaser-video" class="header-video__teaser-video"><source src="'+source+'.webm" type="video/mp4"><source src="'+source+'.mp4" type="video/mp4"></video>';
+    this.$element.append(html);
+};
+
+}
